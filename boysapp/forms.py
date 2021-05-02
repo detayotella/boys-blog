0c475b91c77e94c5715901d4c29f25ee88b5ccee
Tella Adetayo <49725222+detayotella@users.noreply.github.com>
2021-05-02 10:54:28 +0100

    Add files via upload

---
+++ boysapp/forms.py
@@ -0,0 +1,10 @@
+from .models import Comment
+from django import forms
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields =['body']
+        widgets = {
+        'body': forms.Textarea(attrs={'class': 'form-control'})
+        }
